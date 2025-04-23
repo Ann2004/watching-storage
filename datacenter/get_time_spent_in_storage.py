e991d265ac07220a6543f0cd050def16fcf8b283
nyutdf2 <annslob@gmail.com>
2025-04-23 20:41:28 +0300

    delete get_time_spent_in_storage.py

--- datacenter/get_time_spent_in_storage.py
+++
@@ -1,8 +0,0 @@
-from datetime import timedelta
-
-
-def get_time_spent_in_storage(entered_at_time, end_time):
-    delta = end_time - entered_at_time
-    total_seconds = int(delta.total_seconds())
-    delta = timedelta(seconds=total_seconds)
-    return delta
