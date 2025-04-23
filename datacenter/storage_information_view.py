7f2ef9b76fff84b2f49caf3b4dfd02a77eec19c3
nyutdf2 <annslob@gmail.com>
2025-04-23 20:43:53 +0300

    modify file

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,22 +1,18 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils import timezone
-from datacenter.get_time_spent_in_storage import get_time_spent_in_storage
+from .models import get_duration, format_duration
 
 
 def storage_information_view(request):
     unclosed_visits = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = []
     for unclosed_visit in unclosed_visits:
-        entered_at_time = unclosed_visit.entered_at
-        
-        now_time = timezone.localtime(timezone.now())
-        duration = get_time_spent_in_storage(entered_at_time, now_time)
+        duration = format_duration(get_duration(unclosed_visit))
         
         non_closed_visit = {
             'who_entered': unclosed_visit.passcard.owner_name,
-            'entered_at': entered_at_time,
+            'entered_at': unclosed_visit.entered_at,
             'duration': duration,
         }
         non_closed_visits.append(non_closed_visit)
