fde816ff703ad960eb41d0c6369c4884cdbfa012
nyutdf2 <annslob@gmail.com>
2025-06-17 19:41:48 +0300

    fix settings

--- project/settings.py
+++ project/settings.py
@@ -18,11 +18,11 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS',  ['*'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
