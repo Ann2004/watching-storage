b040503eb57b11cd11777622172e2da2c148e075
nyutdf2 <annslob@gmail.com>
2025-06-17 20:28:13 +0300

    fix visit_duration.py

--- datacenter/visit_duration.py
+++ datacenter/visit_duration.py
@@ -1,6 +1,11 @@
 from django.utils import timezone
 
 
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = 3600
+MINUTES_IN_HOUR = 60
+
+
 def get_duration(visit):
     if visit.leaved_at:
         delta = visit.leaved_at - visit.entered_at
@@ -12,8 +17,8 @@ def get_duration(visit):
 
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
+    hours = total_seconds // SECONDS_IN_HOUR
+    minutes = (total_seconds % SECONDS_IN_HOUR) // MINUTES_IN_HOUR
     if hours:
         formatted_time = f'{hours} ч {minutes} мин'
     else:
@@ -23,4 +28,4 @@ def format_duration(duration):
 
 def is_visit_long(visit, minutes=60):
     duration = get_duration(visit)
-    return duration.total_seconds() > minutes * 60
+    return duration.total_seconds() > minutes * SECONDS_IN_MINUTE
